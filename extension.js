be483b9f1552f2f4f0133ab0457ee9f794eb1eef
Christian Bjartli <git@c.bjart.li>
2017-01-15 23:35:47 +0100

    Extension listens to keypress and logs.

--- extension.js
+++ extension.js
@@ -1,81 +1,36 @@
 'use strict';
-// http://mathematicalcoffee.blogspot.no/2012/09/developing-gnome-shell-extensions.html
+
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const Meta = imports.gi.Meta;
 const Tweener = imports.ui.tweener;
 const Lang = imports.lang;
+const Shell = imports.gi.Shell;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
 
-let windowTagger;
-
-
-const WindowMap = new Lang.Class ({
-    Name: 'WindowMap',
-
-    _init: function() {
-        this.windowToKeyMap = {};
-        this.keyToWindowMap = {};
-    },
-
-    rv: Object.freeze({
-        Success: 1,
-        ErrWindowExists: 2,
-        ErrKeyExists: 3
-    }),
-
-    add: function(window, key) {
-        if (this.windowToKeyMap[window] !== undefined) {
-            return this.rv.ErrWindowExists;
-        }
-
-        if (this.keyToWindowMap[key] !== undefined) {
-            return this.rv.ErrKeyExists;
-        }
-
-        this.windowToKeyMap[window] = key;
-        this.keyToWindowMap[key] = window;
-
-        return this.rv.Success;
-    },
-
-    window: function(key) {
-        return this.keyToWindowMap[key] || null;
-    },
-
-    key: function(window) {
-        return this.windowToKeyMap[window] || null;
-    },
-
-    remove_window: function(window) {
-        let key = this.windowToKeyMap[window];
-        delete this.windowToKeyMap[window];
-        delete this.keyToWindowMap[key];
-    },
-
-    remove_key: function(key) {
-        let window = this.keyToWindowMap[key];
-        delete this.keyToWindowMap[key];
-        delete this.windowToKeyMap[window];
-    }
-});
+var windowTagger;
 
 const WindowTagger = new Lang.Class({
     Name: 'WindowTagger',
 
     _init: function() { 
-        this.windowMap = new WindowMap();
+        Main.wm.addKeybinding(
+            'start-tag',
+            Convenience.getSettings(),
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode.NORMAL,
+            () => { log("RECEIVED") }
+        );
     },
 
-    focus: function(key) {
-        let window = this.windowMap.window(key);
-        Main.activateWindow(window);
-    },
-
-    destroy: function() { }
+    destroy: function() { 
+        Main.wm.removeKeybinding('start-tag');
+    }
 });
 
-
-
-function init() { }
+function init() {
+}
 
 function enable() {
     windowTagger = new WindowTagger();
