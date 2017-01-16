a2811011331eecbfa67d5e9d363778a3f595e790
Christian Bjartli <git@c.bjart.li>
2017-01-16 22:39:36 +0100

    MVC

--- extension.js
+++ extension.js
@@ -15,13 +15,47 @@ var windowTagger;
 const WindowTaggerModel = new Lang.Class({
     Name: 'WindowTaggerModel',
 
-    _init: function() { }
+    _init: function() {},
+
+    destroy: function() {}
 });
 
 const WindowTaggerView = new Lang.Class({
     Name: 'WindowTaggerView',
 
-    _init: function() { }
+    _init: function() { 
+        this._hidden = true;
+        this._entry = new St.Entry({style_class: 'text-label', text: "Some text"});
+        this._entry.opacity = 255;
+    },
+
+    getEntry: function() {
+        return this._entry;
+    },
+
+    show: function() {
+        if (this._hidden) {
+            this._hidden = false;
+            let panel = Main.layoutManager.panelBox;
+            let mt = Main.layoutManager.primaryMonitor;
+
+            Main.uiGroup.add_actor(this._entry);
+
+            this._entry.set_position(mt.x + mt.width - this._entry.width, mt.y + panel.height);
+            global.stage.set_key_focus(this._entry);
+        }
+    },
+
+    hide: function() {
+        if (!this._hidden) {
+            this._hidden = true;
+            Main.uiGroup.remove_actor(this._entry);
+        }
+    },
+
+    destroy: function () {
+        this.hide()
+    }
 });
 
 const WindowTaggerController = new Lang.Class({
@@ -30,6 +64,28 @@ const WindowTaggerController = new Lang.Class({
     _init: function(model, view) {
         this._model = model;
         this._view = view;
+
+        Main.wm.addKeybinding(
+            'start-tag',
+            Convenience.getSettings(),
+            Meta.KeyBindingFlags.NONE,
+            Shell.ActionMode.NORMAL,
+            Lang.bind(view, view.show)
+        );
+
+        view.getEntry().connect('key-release-event', (o, e) => { 
+            const ctrl = (e.get_state() & Clutter.ModifierType.CONTROL_MASK) !== 0;
+            const shift = (e.get_state() & Clutter.ModifierType.SHIFT_MASK) !== 0;
+            const sym = e.get_key_symbol();
+
+            if (sym === Clutter.KEY_Escape) {
+                view.hide();
+            }
+        });
+    },
+
+    destroy: function() {
+        Main.wm.removeKeybinding('start-tag');
     }
 });
 
@@ -37,50 +93,15 @@ const WindowTaggerApp = new Lang.Class({
     Name: 'WindowTaggerApp',
 
     _init: function() { 
-        Main.wm.addKeybinding(
-            'start-tag',
-            Convenience.getSettings(),
-            Meta.KeyBindingFlags.NONE,
-            Shell.ActionMode.NORMAL,
-            Lang.bind(this, this._showUI)
-        );
-    },
-
-    _showUI: function() {
-        let panel = Main.layoutManager.panelBox;
-
-        if (!this._label) {
-            this._label = new St.Entry({style_class: 'text-label', text: "Some text"});
-            Main.uiGroup.add_actor(this._label);
-            global.stage.set_key_focus(this._label);
-            this._label.opacity = 255;
-
-            let mt = Main.layoutManager.primaryMonitor;
-            this._label.set_position(mt.x + mt.width - this._label.width, mt.y + panel.height);
-
-            this._label.connect('key-release-event', (o, e) => { 
-                const ctrl = (e.get_state() & Clutter.ModifierType.CONTROL_MASK) !== 0;
-                const shift = (e.get_state() & Clutter.ModifierType.SHIFT_MASK) !== 0;
-                const sym = e.get_key_symbol();
-
-                if (sym === Clutter.KEY_Escape) {
-                    this._hideUI();
-                }
-            });
-
-            //Tweener.addTween(this._label, { opacity: 0, time: 2, transition: 'easeOutQuad', onComplete: Lang.bind(this, this._hideUI) });
-        }
-    },
-
-    _hideUI: function() {
-        if (this._label) {
-            Main.uiGroup.remove_actor(this._label);
-            this._label = null;
-        }
+        this._model = new WindowTaggerModel();
+        this._view = new WindowTaggerView();
+        this._controller = new WindowTaggerController(this._model, this._view);
     },
 
     destroy: function() { 
-        Main.wm.removeKeybinding('start-tag');
+        this._controller.destroy();
+        this._view.destroy();
+        this._model.destroy();
     }
 });
 
